eac94084d463fded91e431a46c009a23c085ed1d
Gilbert787 <olebogengpelle787@gmail.com>
2023-02-16 15:36:36 -0800

    Task 4 puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
- * main -entry point
- * return :Always 0 (success)
+ * Main - Entry point
+ *
+ * Return : Always 0 (success)
  */
 
 int main(void)
 {
-	puts ("\"Programming is like building a multilingual");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
