66c48a72771c70581e2ced1b5213f77c44ba4634
Gilbert787 <olebogengpelle787@gmail.com>
2023-02-18 23:34:04 +0200

    Task 6 print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int number = 0;
+
+	while (number <= 9)
+	{
+		putchar(number);
+		number++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
