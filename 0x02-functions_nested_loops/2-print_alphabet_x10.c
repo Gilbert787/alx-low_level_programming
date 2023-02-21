c01d59198024a9721e565cefd3431c563d6e02f3
Gilbert787 <olebogengpelle787@gmail.com>
2023-02-22 01:32:26 +0200

    Task 2 Print alphabets x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print all alphabets (a to z) 10 times.
+ *
+ * No return
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 0, b;
+
+	while (i < 10)
+	{
+		b = 'a';
+
+		while (b < 'z')
+		{
+			_putchar(b);
+			b++;
+		}
+
+		_putchar('\n');
+		i++;
+	}
+
+}
