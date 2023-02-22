bb949065efaaf35deb00de87db822adb3204383c
Gilbert787 <olebogengpelle787@gmail.com>
2023-02-22 11:23:30 +0200

    Task 3 islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - show if a char is lower or upper case
+ *
+ * @c: int
+ *
+ * Return: @c value
+ */
+
+int _islower(int c)
+{
+	if (c > 96 && c < 123)
+		return (1);
+
+	else
+		return (0);
+
+}
