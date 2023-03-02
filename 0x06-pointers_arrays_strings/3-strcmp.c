043afca244a7349bb263e6f994e5173f4aaca74b
Gilbert787 <olebogengpelle787@gmail.com>
2023-03-02 20:32:02 +0200

    Task 3 Compare strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* _strcmp - Compares 2 strings
+*
+* @s1: string
+*
+* @s2: string
+*
+* Return: return int value
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	int res;
+
+	res = strcmp(s1, s2);
+	return (res);
+
+}
